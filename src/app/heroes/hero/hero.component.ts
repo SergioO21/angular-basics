ebfe3fd65cb7f9d6dfc4b2ac8b93e0f7bacfdd5f
Sergio Orejarena <sergioorueda@gmail.com>
2024-01-27 12:27:42 -0500

    Implementing Modules

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -26,4 +26,9 @@ export class HeroComponent {
     this.age = 25;
   }
 
+  resetForm(): void {
+    this.name = 'ironman';
+    this.age = 45;
+  }
+
 }
--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class ListComponent {
 
+  public heroNames: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor', 'She Hulk'];
+  public deletedHero?: string;
+
+
+  removeLastHero(): void {
+    this.deletedHero = this.heroNames.pop();
+  }
+
+
 }
