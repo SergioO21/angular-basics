d4f61aeb3d5a5fe5e6f2699f0eb2c31c0744e6d7
Sergio Orejarena <sergioorueda@gmail.com>
2024-01-27 20:17:32 -0500

    MainPage Added

---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+
+@Component({
+  selector: 'app-dbz-main-page',
+  templateUrl: './main-page.component.html',
+})
+export class MainPageComponent {
+
+  public characters: Character[] = [
+    {name: 'Krillin', power: 500},
+    {name: 'Goku', power: 100000},
+    {name: 'Vegeta', power: 50000},
+    {name: 'Trunks', power: 1000},
+  ];
+
+}
